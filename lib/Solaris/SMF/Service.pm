e35740d88df5aef24177d8a3bc17da2aa2029b92
Brad Macpherson <brad@cpan.org>
2009-12-01 08:05:21 +0000

    Import of BRAD/Solaris-SMF-0.02 from CPAN.
    gitpan-cpan-distribution: Solaris-SMF
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         BRAD/Solaris-SMF-0.02.tar.gz
    gitpan-cpan-author:       BRAD
    gitpan-cpan-maturity:     released

--- lib/Solaris/SMF/Service.pm
+++ lib/Solaris/SMF/Service.pm
@@ -14,11 +14,11 @@ Solaris::SMF::Service - Encapsulate Solaris 10 services in Perl
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
